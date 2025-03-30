4dfc8bf00e3e87bfa860fafdb1d046b2051ec5fc
sonmmistp <artemijzagzeckij@gmail.com>
2025-03-30 20:51:14 +0300

    done))

---
+++ buttons.py
@@ -0,0 +1,21 @@
+from pyrogram.types import KeyboardButton, InlineKeyboardButton
+
+
+#Фильмы-сериалы, это будут кнопки в клавиатуре, лол
+films_button = KeyboardButton('Фильмы✨')
+serials_button = KeyboardButton('Сериалы📺')
+
+#Деление фильмов на такие "рассы", тип русские и иностранные киношки с сериалами
+russian_button = KeyboardButton('Русские фильмы📺')
+americaburger_button = KeyboardButton('Иностранные фильмы🎱')
+russianserial_button = KeyboardButton('Русские сериалы🪆')
+americaaaaaaakamalatrumploll_button = KeyboardButton('Иностранные сериалы✨')
+
+#Делание inline кнопок со списк
+webb_button = KeyboardButton('Сайты🪆')
+webam_button = KeyboardButton('Сайты🌸')
+amweb_button = KeyboardButton('Сайты🍡')
+rusweb_button = KeyboardButton('Сайты🎆')
+
+#делаем кнопку, которая возращает на начало
+undo_button = KeyboardButton('Назад🔙')
---
+++ keyboards.py
@@ -0,0 +1,38 @@
+from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup
+
+import buttons as b
+
+main_keyboard = ReplyKeyboardMarkup([
+    [b.films_button],
+    [b.serials_button]
+], resize_keyboard=True)
+
+chooseuridklol_keyboard = ReplyKeyboardMarkup([
+    [b.russian_button],
+    [b.americaburger_button]
+], resize_keyboard=True)
+
+trumpkamalabillieszakendrick_keyboard = ReplyKeyboardMarkup([
+    [b.russianserial_button],
+    [b.americaaaaaaakamalatrumploll_button]
+], resize_keyboard=True)
+
+websiterusserial_keyboard = ReplyKeyboardMarkup([
+    [b.webb_button]
+], resize_keyboard=True)
+
+websiteammserial_keyboard = ReplyKeyboardMarkup([
+    [b.webam_button]
+], resize_keyboard=True)
+
+websiteamfilm_keyboard = ReplyKeyboardMarkup([
+    [b.amweb_button]
+], resize_keyboard=True)
+
+webrusfilm_keyboard = ReplyKeyboardMarkup([
+    [b.rusweb_button]
+], resize_keyboard=True)
+
+undo_keyboard = ReplyKeyboardMarkup([
+    [b.undo_button]
+], resize_keyboard=True)
